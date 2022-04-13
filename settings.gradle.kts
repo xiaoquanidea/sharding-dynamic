eabe9beb353ba53abb493fc88344e85f5a55e83d
xiao <xiaoquanidea@163.com>
2022-04-13 19:26:56 +0800

    修改settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,4 @@ include("sharding-starter")
 include("sharding-samples")
 include("sharding-xa")
 include("sharding-samples:sharding-jta-sample")
-findProject(":sharding-samples:sharding-jta-sample")?.name = "sharding-jta-sample"
 include("sharding-samples:sharding-nonjta-sample")
-findProject(":sharding-samples:sharding-nonjta-sample")?.name = "sharding-nonjta-sample"
